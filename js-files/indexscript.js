18bd90cc4a8aa5554614bd99b8749f8d4096dcfa
JoWi235 <jonas.wilmar23@gmail.com>
2024-12-07 22:20:06 +0100

    automatische alter berechnung

---
+++ js-files/indexscript.js
@@ -0,0 +1,24 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const myBirthday = "2009-05-23";
+    document.getElementById("age").innerHTML = calculateMyAge(myBirthday);
+
+    function calculateMyAge(birthday) {
+        const toDay = new Date();
+        const birthDate = new Date(birthday);
+        let age = toDay.getFullYear() - birthDate.getFullYear();
+        const birthdayThisYear = new Date(
+            toDay.getFullYear(),
+            birthDate.getMonth(),
+            birthDate.getDate()
+        );
+
+        if (toDay < birthdayThisYear) {
+            age--;
+        }
+
+        return age;
+    }
+});
+
+
